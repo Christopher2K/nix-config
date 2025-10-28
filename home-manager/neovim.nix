29d165e0404bca371ed802bb445a56cc9b262f8d
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2025-10-28 10:53:57 -0400

    feat: nvim config - part 2

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,13 +1,21 @@
- {
-   pkgs,
-   neovim-nightly-overlay,
-   ...
- } : {
-   programs.neovim = {
-     enable = true;
-     package = neovim-nightly-overlay.packages.${pkgs.system}.default;
-     defaultEditor = true;
-     viAlias = true;
-     vimAlias = true;
-   };
- }
+{
+  pkgs,
+  neovim-nightly-overlay,
+  getDest,
+  getConfig,
+  ...
+}:
+{
+  home.file."${getDest ".config/nvim"}" = {
+    source = getConfig "nvim";
+    recursive = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    package = neovim-nightly-overlay.packages.${pkgs.system}.default;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+}
