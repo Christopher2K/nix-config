da4ec50713c49adb577c23ddc7fff3fa7a596d56
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2025-12-22 18:09:25 -0500

    fix: packages

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -4,34 +4,39 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    # CLI
-    bat
-    beamMinimal28Packages.rebar3
-    claude-code
-    codex
-    devenv
-    exercism
-    fd
-    fzf
-    glow
-    lazydocker
-    lazygit
-    lf
-    neofetch
-    nixfmt-rfc-style
-    opencode
-    ripgrep
-    scrcpy
-    tree-sitter
-    watchman
+  home.packages =
+    with pkgs;
+    [
+      # CLI
+      bat
+      beamMinimal28Packages.rebar3
+      claude-code
+      codex
+      devenv
+      exercism
+      fd
+      fzf
+      glow
+      lazydocker
+      lazygit
+      lf
+      neofetch
+      nixfmt-rfc-style
+      opencode
+      ripgrep
+      scrcpy
+      tree-sitter
+      watchman
 
-    # GUI
-    yaak
-    zoom-us
-  ] ++ lib.optionals stdenv.isDarwin [
-    orbstack
-    tableplus
-    vlc-bin-universal
-  ];
+      # GUI
+      yaak
+      zoom-us
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      orbstack
+      tableplus
+      vlc-bin-universal
+    ]
+    ++ lib.optionals stdenv.isLinux [
+    ];
 }
