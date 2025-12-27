3261feda0fd2a1b0b4e37d94551fa5eea781ced6
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2025-12-27 13:54:44 -0500

    feat: simplify video configuration

--- configuration/nixos/nvidia.nix
+++ configuration/nixos/nvidia.nix
@@ -5,6 +5,8 @@
   boot.kernelParams = [
     "amdgpu.modeset=1"
     "nvidia_drm.modeset=1"
+    "nvidia_drm.fbdev=1"
+    "nvidia.NVreg_TemporaryFilePath=/var/tmp"
   ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -20,13 +22,7 @@
       open = true;
       modesetting.enable = true;
       powerManagement.enable = true;
-      powerManagement.finegrained = true;
-      prime = {
-        offload.enable = true;
-        offload.enableOffloadCmd = true;
-        nvidiaBusId = "PCI:197:0:0";
-        amdgpuBusId = "PCI:198:0:0";
-      };
+      powerManagement.finegrained = false;
     };
   };
 }
