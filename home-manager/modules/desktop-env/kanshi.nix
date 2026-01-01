bd0d440bf8e6325876f86fa9666d138abfe0436d
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2026-01-01 17:10:28 -0500

    fix: position

--- home-manager/modules/desktop-env/kanshi.nix
+++ home-manager/modules/desktop-env/kanshi.nix
@@ -64,11 +64,13 @@ in
             criteria = monitors.builtin-laptop;
             scale = 1.5;
             mode = "2560x1600@240";
+            position = "0,0";
           }
           {
             criteria = monitors.lg-home;
             scale = 1.0;
             mode = "2560x1440@120";
+            position = "2560,0";
           }
         ];
       }
