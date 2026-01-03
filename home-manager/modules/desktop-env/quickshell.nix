5b9688e74c9714d291d5720e697b16a1ed424e5f
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2026-01-02 20:39:24 -0500

    feat: add more quickshell deps

--- home-manager/modules/desktop-env/quickshell.nix
+++ home-manager/modules/desktop-env/quickshell.nix
@@ -8,9 +8,14 @@
 }:
 {
   home.packages = with pkgs; [
-    inputs.quickshell.packages.${stdenv.hostPlatform.system}.default
+    inputs.qml-niri.packages.${stdenv.hostPlatform.system}.quickshell
     kdePackages.qtdeclarative
+    qt6.qt5compat
   ];
+
+  home.sessionVariables = {
+    QML_IMPORT_PATH = "${pkgs.qt6.qt5compat}/lib/qt-6/qml";
+  };
 
   home.file."${configDest "quickshell"}".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/files/quickshell";
