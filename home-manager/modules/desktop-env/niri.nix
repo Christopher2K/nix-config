62ca28ac02b8651d527e4db8045ea4fab0828bfb
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2026-01-02 23:11:52 -0500

    feat: shortcut clean

--- home-manager/modules/desktop-env/niri.nix
+++ home-manager/modules/desktop-env/niri.nix
@@ -244,23 +244,10 @@
           action.close-window = [ ];
         };
 
-        # Focus navigation (arrows)
-        "Mod+Left".action.focus-column-left = [ ];
-        "Mod+Down".action.focus-window-down = [ ];
-        "Mod+Up".action.focus-window-up = [ ];
-        "Mod+Right".action.focus-column-right = [ ];
-
-        # Focus navigation (vim keys)
         "Mod+H".action.focus-column-left = [ ];
         "Mod+J".action.focus-window-down = [ ];
         "Mod+K".action.focus-window-up = [ ];
         "Mod+L".action.focus-column-right = [ ];
-
-        # Move windows (arrows)
-        "Mod+Ctrl+Left".action.move-column-left = [ ];
-        "Mod+Ctrl+Down".action.move-window-down = [ ];
-        "Mod+Ctrl+Up".action.move-window-up = [ ];
-        "Mod+Ctrl+Right".action.move-column-right = [ ];
 
         # Move windows (vim keys)
         "Mod+Ctrl+H".action.move-column-left = [ ];
@@ -274,23 +261,11 @@
         "Mod+Ctrl+Home".action.move-column-to-first = [ ];
         "Mod+Ctrl+End".action.move-column-to-last = [ ];
 
-        # Monitor focus (arrows)
-        "Mod+Shift+Left".action.focus-monitor-left = [ ];
-        "Mod+Shift+Down".action.focus-monitor-down = [ ];
-        "Mod+Shift+Up".action.focus-monitor-up = [ ];
-        "Mod+Shift+Right".action.focus-monitor-right = [ ];
-
         # Monitor focus (vim keys)
         "Mod+Shift+H".action.focus-monitor-left = [ ];
         "Mod+Shift+J".action.focus-monitor-down = [ ];
         "Mod+Shift+K".action.focus-monitor-up = [ ];
         "Mod+Shift+L".action.focus-monitor-right = [ ];
-
-        # Move column to monitor (arrows)
-        "Mod+Shift+Ctrl+Left".action.move-column-to-monitor-left = [ ];
-        "Mod+Shift+Ctrl+Down".action.move-column-to-monitor-down = [ ];
-        "Mod+Shift+Ctrl+Up".action.move-column-to-monitor-up = [ ];
-        "Mod+Shift+Ctrl+Right".action.move-column-to-monitor-right = [ ];
 
         # Move column to monitor (vim keys)
         "Mod+Shift+Ctrl+H".action.move-column-to-monitor-left = [ ];
@@ -316,36 +291,6 @@
         "Mod+Shift+U".action.move-workspace-down = [ ];
         "Mod+Shift+I".action.move-workspace-up = [ ];
 
-        # Mouse wheel workspace navigation
-        "Mod+WheelScrollDown" = {
-          cooldown-ms = 150;
-          action.focus-workspace-down = [ ];
-        };
-        "Mod+WheelScrollUp" = {
-          cooldown-ms = 150;
-          action.focus-workspace-up = [ ];
-        };
-        "Mod+Ctrl+WheelScrollDown" = {
-          cooldown-ms = 150;
-          action.move-column-to-workspace-down = [ ];
-        };
-        "Mod+Ctrl+WheelScrollUp" = {
-          cooldown-ms = 150;
-          action.move-column-to-workspace-up = [ ];
-        };
-
-        # Mouse wheel column navigation
-        "Mod+WheelScrollRight".action.focus-column-right = [ ];
-        "Mod+WheelScrollLeft".action.focus-column-left = [ ];
-        "Mod+Ctrl+WheelScrollRight".action.move-column-right = [ ];
-        "Mod+Ctrl+WheelScrollLeft".action.move-column-left = [ ];
-
-        # Shift + wheel for horizontal scrolling
-        "Mod+Shift+WheelScrollDown".action.focus-column-right = [ ];
-        "Mod+Shift+WheelScrollUp".action.focus-column-left = [ ];
-        "Mod+Ctrl+Shift+WheelScrollDown".action.move-column-right = [ ];
-        "Mod+Ctrl+Shift+WheelScrollUp".action.move-column-left = [ ];
-
         # Workspace by number
         "Mod+1".action.focus-workspace = 1;
         "Mod+2".action.focus-workspace = 2;
@@ -355,7 +300,6 @@
         "Mod+6".action.focus-workspace = 6;
         "Mod+7".action.focus-workspace = 7;
         "Mod+8".action.focus-workspace = 8;
-        "Mod+9".action.focus-workspace = 9;
 
         # Move column to workspace by number
         "Mod+Ctrl+1".action.move-column-to-workspace = 1;
@@ -366,7 +310,6 @@
         "Mod+Ctrl+6".action.move-column-to-workspace = 6;
         "Mod+Ctrl+7".action.move-column-to-workspace = 7;
         "Mod+Ctrl+8".action.move-column-to-workspace = 8;
-        "Mod+Ctrl+9".action.move-column-to-workspace = 9;
 
         # Column management
         "Mod+BracketLeft".action.consume-or-expel-window-left = [ ];
