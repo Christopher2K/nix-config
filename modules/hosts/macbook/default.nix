dfba21c8e8e8892fc05436df91dc9e6ec7a342b2
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2026-04-01 23:31:06 -0400

    feat: darwin starting point

--- modules/hosts/macbook/default.nix
+++ modules/hosts/macbook/default.nix
@@ -1,1 +1,26 @@
-{ }
+{ inputs, config, ... }:
+let
+  universal = config.flakes.universal;
+  darwin = config.flake.modules.darwin;
+  hm = config.flake.modules.homeManager;
+in
+{
+  flake.darwinConfigurations.macbook = inputs.nix-darwin.lib.darwinSystem {
+    modules = [
+      # system level configuration
+
+      # home level configuration
+      inputs.nix-homebrew.darwinModules.nix-homebrew
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.${config.flake.username} = {
+          imports = [
+            hm.christopher
+          ];
+        };
+      }
+    ];
+  };
+}
